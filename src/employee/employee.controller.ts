6eb82361a09c14b329946ec195e7063a215ae09a
quang <quangb2016996@student.ctu.edu.vn>
2025-06-23 16:07:52 +0700

    Recommit the previous one

--- src/employee/employee.controller.ts
+++ src/employee/employee.controller.ts
@@ -1,5 +1,5 @@
 // Commons
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards, ValidationPipe} from '@nestjs/common';
 
 // Dependencies
 import { EmployeeService } from './employee.service';
@@ -8,18 +8,18 @@ import { Employee } from './employee.entity';
 // Responses
 import { ResponseDto } from 'src/common/dto/response.dto';
 import { errorResponse, successResponse, successWithDataResponse } from 'src/common/helpers/response.helper';
-import { EmployeeGuard } from './employee.guard';
+import { CreatePayloadDTO } from './DTOs/employee.dto';
+import { createPipe } from './employee.pipe';
 
 @Controller('employee')
-@UseGuards(EmployeeGuard)
 export class EmployeeController {
     constructor (private readonly employeeService:EmployeeService){}
 
     @Post('create')
-    async createEmployee(@Body() payload:Employee): Promise <ResponseDto<Employee | null>>{
+    async createEmployee(@Body(createPipe) payload:CreatePayloadDTO ): Promise <ResponseDto<Employee | null>>{
 
-        if (await this.employeeService.getEmployeeByEmail(payload.email)) return errorResponse('Email is used by another account', 400)
-        
+        if (await this.employeeService.getEmployeeByEmail(payload.email)) return errorResponse('Invalid input: Email is used by another account', 400)
+            
         try {
             await this.employeeService.createEmployee(payload)
             return successWithDataResponse(payload, 'Created', 201)
